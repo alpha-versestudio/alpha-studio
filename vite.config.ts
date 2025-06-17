ca5e6e678d065624bf8d95f71ca2582980e675d2
alpha-versestudio <alphaduhaim@gmail.com>
2025-06-17 13:33:58 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: '/alpha-studio/',
+  server: {
+    host: "::",
+    port: 8080,
+    strictPort: true,
+    open: true,
+    fs: {
+      strict: false,
+    },
+  },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
+      }
+    }
+  },
+  assetsInclude: ['**/*.pdf'],
+}));
